874ad9ab224b1ce1df59585cea1812dd006b5ad8
Marvin JEAN <E174824B@I012V7pc06.ensinfo.sciences.univ-nantes.prive>
2017-11-10 13:32:59 +0100

    requemodif

--- SQL/reqs.sql
+++ SQL/reqs.sql
@@ -2,11 +2,10 @@ prompt *************************************************************
 prompt ********************** REQUESTS *****************************
 prompt *************************************************************
 
--- peu interessante
--- prompt **** Affichage des films pour enfants
--- SELECT fi.title
--- FROM fait fa, d_film fi, d_genre ge
--- WHERE fa.d_genre_id=ge.id AND fa.d_film_id = fi.id AND ge.adult = 0;
+prompt **** Affichage des films pour enfants
+SELECT fi.title
+FROM fait fa, d_film fi, d_genre ge
+WHERE fa.d_genre_id=ge.id AND fa.d_film_id = fi.id AND ge.adult = 0;
 
 prompt **** Affichage des genres produit par les compagnies ainsi que les entrées total engndré par ce genre
 SELECT ge.genre_name, co.name_, sum(fa.admissions) AS "nb_entree"
@@ -38,14 +37,21 @@ FROM fait f, d_time dt, d_genre dg
 WHERE f.d_time_id = dt.id and f.d_genre_id = dg.id
 GROUP BY CUBE(dt.month, dg.genre_name);
 
-prompt **** Categorisation des pays où sont produit les films generant le plus de revenu par an avec leur films
-SELECT zo.production_country, ti.month NTILE(5) over(order by sum(fa.revenue) desc)
+prompt **** Categorisation des pays où sont produit les films generant le plus de revenu par ans avec leur films
+SELECT zo.production_country, ti.month, NTILE(5) over(order by sum(fa.revenue) desc) AS "group"
 FROM fait fa, d_zone zo, d_time ti
 WHERE fa.d_zone_id = zo.id AND fa.d_time_id = ti.id
-GROUP BY zo.production_country;
+GROUP BY ti.year
+
 
 prompt **** cumul des budget des films francais depuis 2000
-SELECT ti.years, sum(fa.budget), sum(sum(fa.budget)) over(order by ti.years rows unbounded preceding)
-FROM fait fa, d_time, d_zone zo
-WHERE fa.d_time_id = ti.id AND ti.years >= 2000 AND zo.original_language = "fr"
-GROUP BY ti.years;
+SELECT ti.year, sum(fa.budget) AS, sum(sum(fa.budget)) over(order by ti.year rows unbounded preceding)
+FROM fait fa, d_time ti, d_zone zo
+WHERE fa.d_time_id = ti.id AND ti.year >= 2000 AND zo.original_language = 'fr'
+GROUP BY ti.year;
+
+prompt **** Classement des genre qui engendre un temps moyen a l affiche le plus long
+SELECT ge.genre_name,avg(fa.runtime) AS "Moyenne de temps en salle (j)", rank() over (order by avg(fa.runtime) desc) AS "Rang"
+FROM fait fa, d_genre ge
+WHERE fa.d_genre_id = ge.id
+GROUP BY ge.genre_name;
