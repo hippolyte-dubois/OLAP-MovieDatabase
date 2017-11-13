df2088d3b117399381724129d8c172af9506a6f9
Roxane BELLOT <E166292C@I011V7pc10.ensinfo.sciences.univ-nantes.prive>
2017-11-13 17:14:10 +0100

    début requetes dans rapport

--- SQL/reqs.sql
+++ SQL/reqs.sql
@@ -11,7 +11,7 @@ FROM fait fa, d_film fi, d_genre ge
 WHERE fa.d_genre_id=ge.id AND fa.d_film_id = fi.id AND ge.adult = 1;
 
 
-prompt **** Affichage des genres produit par les compagnies ainsi que les entrées total engndré par ce genre
+prompt **** Entées en fonction des compagnies et du genre
 SELECT ge.genre_name, co.name_, sum(fa.admissions) AS "nb_entree"
 FROM fait fa, d_genre ge, d_company co
 WHERE fa.d_genre_id = ge.id AND fa.d_company_id = co.id AND ge.genre_name !='NULL' AND co.name_ !='NULL'
@@ -19,7 +19,7 @@ GROUP BY ROLLUP(ge.genre_name,co.name_);
 
 
 prompt ****
-prompt **** TOP 10 des films qui ont le plus revenue
+prompt **** TOP 10 des films qui ont généré le plus de revenu
 prompt ****
 SELECT fi.title
 FROM d_film fi, fait fa
@@ -28,20 +28,20 @@ ORDER BY fa.revenue;
 
 
 prompt ****
-prompt **** entrées par genres en fonction de l’année
+prompt **** Entrées par genres en fonction de l’année
 prompt ****
 SELECT dt.year, dg.genre_name, SUM(f.revenue)
 FROM fait f, d_time dt, d_genre dg
-WHERE f.d_time_id = dt.id and f.d_genre_id = dg.id
+WHERE f.d_time_id = dt.id AND f.d_genre_id = dg.id
 GROUP BY CUBE (dt.year, dg.genre_name);
 
 
 prompt ****
-prompt **** réussite moyenne (entrées, revenu, popularité, vote average) et nombre de films des boites de production, par trimestre
+prompt **** réussite moyenne (entrées, revenu, popularité, vote moyen) et nombre de films des boites de production, par trimestre
 prompt ****
 SELECT dt.season, dt.year, dc.name_, AVG(f.admissions), AVG(f.popularity), AVG(f.revenue), AVG(f.vote_average), count(f.id)
 FROM fait f, d_time dt, d_company dc
-WHERE f.d_time_id = dt.id and f.d_company_id = dc.id
+WHERE f.d_time_id = dt.id AND f.d_company_id = dc.id
 GROUP BY ROLLUP(dc.name_, dt.year, dt.season);
 
 
@@ -50,7 +50,7 @@ prompt **** nombre de films qui sortent par mois et par genre
 prompt ****
 SELECT dt.month, dg.genre_name, count(f.id)
 FROM fait f, d_time dt, d_genre dg
-WHERE f.d_time_id = dt.id and f.d_genre_id = dg.id
+WHERE f.d_time_id = dt.id AND f.d_genre_id = dg.id
 GROUP BY CUBE(dt.month, dg.genre_name);
 
 
@@ -59,7 +59,7 @@ prompt **** la rentabilité par genre, par année et par boite
 prompt ****
 SELECT dc.name_, dt.year, dg.genre_name, SUM(f.revenue) - SUM(f.budget) as "profit"
 FROM fait f, d_company dc, d_time dt, d_genre dg
-WHERE f.d_company_id = dc.id and f.d_time_id = dt.id and f.d_genre_id = dg.id
+WHERE f.d_company_id = dc.id AND f.d_time_id = dt.id and f.d_genre_id = dg.id
 GROUP BY GROUPING SETS((dc.name_, dt.year, dg.genre_name), dc.name_);
 
 
@@ -86,10 +86,10 @@ GROUP BY GROUPING SETS((dc.name_, dt.year, dg.genre_name), dc.name_);
 prompt ****
 prompt **** Categorisation des pays où sont produit les films generant le plus de revenu par an avec leur films
 prompt ****
-SELECT zo.production_country, ti.month, NTILE(5) over(order by sum(fa.revenue) desc) AS "group"
+SELECT zo.production_country, ti.year, NTILE(5) over(order by sum(fa.revenue) desc) AS "group"
 FROM fait fa, d_zone zo, d_time ti
 WHERE fa.d_zone_id = zo.id AND fa.d_time_id = ti.id
-GROUP BY ti.year
+GROUP BY zo.production_country, ti.year;
 
 
 prompt **** cumul des budget des films francais depuis 2000
