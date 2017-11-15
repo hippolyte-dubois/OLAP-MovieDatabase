ba1c6df690c5e4d1f4f51217ac965eee84247570
Roxane BELLOT <E166292C@I004pc13.ensinfo.sciences.univ-nantes.prive>
2017-11-15 08:09:52 +0100

    remove useless comments

--- SQL/seqs.sql
+++ SQL/seqs.sql
@@ -1,34 +1,4 @@
--- CREATE SEQUENCE id_d_zone;
--- CREATE SEQUENCE id_d_time;
--- CREATE SEQUENCE id_d_film;
 CREATE SEQUENCE id_fait;
-
-
--- CREATE OR REPLACE TRIGGER d_zone_on_insert
---   BEFORE INSERT ON d_zone
---   FOR EACH ROW
--- BEGIN
---   SELECT id_d_zone.nextval
---   INTO :new.id
---   FROM dual;
--- END;
--- /
---
--- CREATE OR REPLACE TRIGGER d_time_on_insert
--- BEFORE INSERT ON d_time
--- FOR EACH ROW
--- BEGIN
--- SELECT id_d_time.nextval INTO :new.id FROM dual;
--- END;
--- /
---
--- CREATE OR REPLACE TRIGGER d_film_on_insert
--- BEFORE INSERT ON d_film
--- FOR EACH ROW
--- BEGIN
--- SELECT id_d_film.nextval INTO :new.id FROM dual;
--- END;
-/
 
 CREATE OR REPLACE TRIGGER fait_on_insert
 BEFORE INSERT ON fait
