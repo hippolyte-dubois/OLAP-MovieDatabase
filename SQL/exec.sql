857bc524110fb459ea060c5e30d347794d3d580d
Roxane BELLOT <E166292C@I004pc13.ensinfo.sciences.univ-nantes.prive>
2017-11-15 08:09:13 +0100

    add right to user admi45

--- SQL/exec.sql
+++ SQL/exec.sql
@@ -2,5 +2,6 @@ set sqlblanklines on
 set lines 150
 start drop
 start create
+start grant
 start inserts
 start reqs
---
+++ SQL/grant.sql
@@ -0,0 +1,6 @@
+GRANT SELECT ON d_zone TO admi45;
+GRANT SELECT ON d_time TO admi45;
+GRANT SELECT ON d_genre TO admi45;
+GRANT SELECT ON d_film TO admi45;
+GRANT SELECT ON d_company TO admi45;
+GRANT SELECT ON fait TO admi45;
